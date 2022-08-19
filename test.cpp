1f6b111470c1efe2414aa666fc5c6d304c7e42ce
fake-rookie <61480354+fake-rookie@users.noreply.github.com>
2022-08-19 10:57:31 +0800

    update to c++17

--- test.cpp
+++ test.cpp
@@ -1,8 +1,6 @@
 #include <cstdio>
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
-
 int main(int argc, char **argv)
 {
     if (argc != 2) {
@@ -10,18 +8,17 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    Mat image;
-    image = imread(argv[1], 1);
+    cv::Mat image {cv::imread(argv[1], 1)};
 
     if (!image.data) {
         printf("No image data.\n");
         return -1;
     }
 
-    namedWindow("Display Image", WINDOW_AUTOSIZE);
-    imshow("Display Image", image);
+    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Display Image", image);
 
-    waitKey(0);
+    cv::waitKey(0);
 
     return 0;
 }
